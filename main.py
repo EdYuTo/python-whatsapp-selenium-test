457ca11368f415c95d2fe3dd8075dcfbbe64484f
EdYuTo <edson.yudi.toma@gmail.com>
2023-01-23 17:09:18 -0300

    Add sticker

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 import time
 
+# TODO: extract with dependencies
+def __waitForView(by=By.ID, value: str = None):
+    wait = WebDriverWait(driver, 600)
+    return wait.until(EC.presence_of_element_located((by, value)))
+
 # -- #
 # customization
 target = 'target'
-message = 'message'
 number_of_times = 2
 delay_in_seconds = 5
 user_data_dir = 'chrome://version/ to check your profile path'
@@ -25,25 +29,27 @@ options.add_argument(f'user-data-dir={user_data_dir}')
 # init browser
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 driver.get("https://web.whatsapp.com/")
-wait = WebDriverWait(driver, 600)
 
 # search target
 # search_box = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'Er7QU')))
-search_box = wait.until(EC.presence_of_element_located((By.XPATH, '//div[@title="Search input textbox"]')))
+search_box = __waitForView(By.XPATH, '//div[@title="Search input textbox"]')
 time.sleep(1)
 search_box.click()
 search_box.send_keys(target)
 
 # interact with target
 # contact = search_box = wait.until(EC.presence_of_element_located((By.CLASS_NAME, '_1Oe6M')))
-contact = wait.until(EC.presence_of_element_located((By.XPATH, f"//span[@title='{target}']")))
+contact = __waitForView(By.XPATH, f"//span[@title='{target}']")
 time.sleep(1)
 contact.click()
 
-# write text
-message_box = driver.find_element(By.XPATH, "//div[@title='Type a message']")
+# send sticker
+# driver.find_element(By.XPATH, '//button[@aria-label="Open emojis panel"]').click()
+__waitForView(By.XPATH, '//span[@data-testid="smiley"]').click()
+__waitForView(By.XPATH, '//span[@data-testid="sticker"]').click()
+sticker = __waitForView(By.XPATH, '//img[@class="_3t1hO _1aShU ZRhsD"]') # set your sticker
 for i in range(number_of_times):
-    message_box.send_keys(message + Keys.ENTER)
+    sticker.click()
     if i != number_of_times - 1:
         time.sleep(delay_in_seconds)
     else:
