1181e8fd0c63c8ce71b95e691a056b14eedc96fd
EdYuTo <edson.yudi.toma@gmail.com>
2023-07-19 23:25:33 -0300

    Add funcs and custom timeout

--- src/custom_chrome_driver.py
+++ src/custom_chrome_driver.py
@@ -16,18 +16,24 @@ class CustomChromeDriver:
 
         return options
 
-    def __init__(self, user_data_dir: str=None, profile_directory: str=None):
+    def __init__(self, user_data_dir: str=None, profile_directory: str=None, timeout: int=60):
         options = self.__config(user_data_dir, profile_directory)
 
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-        self.wait = WebDriverWait(self.driver, 60)
+        self.wait = WebDriverWait(self.driver, timeout)
 
     def load_url(self, url):
         self.driver.get(url)
     
+    def switch_to_tab(self, tab):
+        self.driver.switch_to.window(self.driver.window_handles[tab])
+    
     def wait_for_element(self, by=By.ID, value: str=None):
         return self.wait.until(EC.presence_of_element_located((by, value)))
     
+    def find_elements(self, by=By.ID, value: str=None):
+        return self.driver.find_elements(by, value)
+
     def find_element(self, by=By.ID, value: str=None):
         return self.driver.find_element(by, value)
 
